e999b2336cf01c81a734716e0ab55198913e36d4
TheGreatFruit <pierre.28.gauthier@gmail.com>
2026-03-26 19:25:29 +0100

    Completed Cas7

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,20 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		StringBuilder chaine = new StringBuilder();
+		if(infosMarche.length == 0) {
+			chaine.append("Le marché est vide, revenez plus tard.\n");
+		} else {
+			chaine.append(nomAcheteur + " vous trouverez au marché :\n");
+			for(int i=0; i<infosMarche.length;i++) {
+				chaine.append("- " + infosMarche[i]);
+				i++;
+				chaine.append(" qui vend " + infosMarche[i]);
+				i++;
+				chaine.append(infosMarche[i] + ".\n");
+			}
+		}
+		chaine.toString();
 	}
 }
